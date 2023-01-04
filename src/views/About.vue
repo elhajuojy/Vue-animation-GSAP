712598e23628cc886b7dd111e6df4b4353d145e2
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-04 18:47:06 +0100

    gsap.from

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,6 @@
      name="fade"
      @before-enter="beforeEnter"
      @enter="enter"
-     @after-enter="afterEnter"
-     @before-leave="beforeLeave"
-     @leave="leave"
-     @after-leave="afterLeave"
      >
       <h1 >About</h1>
     </transition>
@@ -16,31 +12,40 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
   </div>
+  <div class="content" ref="content">
+    <h1>Headling Goes Here </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam delectus officia dolorem obcaecati voluptate accusamus sed id omnis asperiores voluptatibus. Corrupti excepturi fuga iusto quis iste sint nulla vel rem.
+    </p>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref,onMounted } from 'vue'
 import gsap from 'gsap'
 
 export default {
 
   setup() {
+    const content = ref(null)
     const beforeEnter = (el)=>{
-        console.log(el);
-        el.style.transform = "translateY(-60px)"
-        el.style.opacity = 0;
+      
     }
-    const enter = (el,done)=>{
-      gsap.to(el,{
+
+    onMounted(() => {
+      gsap.from(content.value,{
+        delay:0.5,
         duration:1,
-        y:0,
-        opacity:1,
-        ease:"bounce.out",
-        //we can't move on until we done that's we use this on complete
-        onComplete : done
+        y:"+100",
+        autoAlpha:0,
+        ease:"back.out(1.7)"
       })
+    });
+
+    const enter = (el,done)=>{
+      
     }
-    return {beforeEnter,enter}
+    return {beforeEnter,enter,content}
   }
 }
 </script>
