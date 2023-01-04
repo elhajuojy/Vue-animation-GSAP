e96d44e7097f976b4c113df7dff47aa39dbaeb5a
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-04 14:01:20 +0100

    Animations  - Using GSAP

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition
+     appear 
+     name="fade"
+     @before-enter="beforeEnter"
+     @enter="enter"
+     @after-enter="afterEnter"
+     @before-leave="beforeLeave"
+     @leave="leave"
+     @after-leave="afterLeave"
+     >
+      <h1 >About</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,15 +19,34 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+import gsap from 'gsap'
 
 export default {
-  setup() {
 
+  setup() {
+    const beforeEnter = (el)=>{
+        console.log(el);
+        el.style.transform = "translateY(-60px)"
+        el.style.opacity = 0;
+    }
+    const enter = (el,done)=>{
+      gsap.to(el,{
+        duration:1,
+        y:0,
+        opacity:1,
+        ease:"bounce.out",
+        //we can't move on until we done that's we use this on complete
+        onComplete : done
+      })
+    }
+    return {beforeEnter,enter}
   }
 }
 </script>
 
 <style>
+  
   .about {
     max-width: 600px;
     margin: 20px auto;
